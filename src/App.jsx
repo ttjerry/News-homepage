e23ef128670582ee9d52e8d824373d4cad1d0e22
vivi-uch <vivianokonkwouch>
2024-09-07 13:24:20 +0100

    check the comments in Nav.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,19 +23,19 @@ const footer = [
     cardImage: "/assets/images/image-retro-pcs.jpg",
     number: "01",
     text1: "Revivng Retro PCs",
-    details: 'What happens when old PCs are given modern upgrades?'
+    details: "What happens when old PCs are given modern upgrades?",
   },
   {
     cardImage: "/assets/images/image-top-laptops.jpg",
     number: "02",
     text1: "Top 10 Laptops of 2022",
-    details: 'Our best picks for various needs and budgets.'
+    details: "Our best picks for various needs and budgets.",
   },
   {
     cardImage: "/assets/images/image-gaming-growth.jpg",
     number: "03",
     text1: "The growth of Gaming",
-    details: 'How the pandemic has sparked fresh opportunities.'
+    details: "How the pandemic has sparked fresh opportunities.",
   },
 ];
 
@@ -44,31 +44,40 @@ function App() {
     <div>
       <Nav />
       <Top />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
 
 export default App;
 
-
 function Top() {
   return (
     <div className="flex lg:flex-row lg:px-24 flex-col gap-8 px-5">
       <div className="flex flex-col lg:w-8/12 gap-2">
-        <img src="/assets/images/image-web-3-mobile.jpg" className="lg:hidden flex" alt="" />
-        <img src="/assets/images/image-web-3-desktop.jpg" alt="" className="hidden lg:flex lg:h-4/6"  />
+        <img
+          src="/assets/images/image-web-3-mobile.jpg"
+          className="lg:hidden flex"
+          alt=""
+        />
+        <img
+          src="/assets/images/image-web-3-desktop.jpg"
+          alt=""
+          className="hidden lg:flex lg:h-4/6"
+        />
         <span className="flex flex-col gap-8 lg:pt-8 lg:gap-16 lg:h-2/6 h-4/6 lg:flex-row lg:justify-around items-start">
-          <h1 className="font-bold lg:text-4xl text-4xl">The Bright Future of Web 3.0</h1>
+          <h1 className="font-bold lg:text-4xl text-4xl">
+            The Bright Future of Web 3.0
+          </h1>
           <span className="lg:flex lg:h-full h-full justify-between lg:justify-between lg:flex-col">
-          <h3 className="text-sm text-gray-400">
-            We dive into the next evolution of thee web that claims to put the
-            power of the platforms back into the hands of the people. But is it
-            really fulfilling its promise?
-          </h3>
-          <button className="bg-red-500 relative top-4 lg:top-0 lg:self-start justify-end p-2 px-5 text-sm font-bold">
-            READ MORE
-          </button>
+            <h3 className="text-sm text-gray-400">
+              We dive into the next evolution of thee web that claims to put the
+              power of the platforms back into the hands of the people. But is
+              it really fulfilling its promise?
+            </h3>
+            <button className="bg-red-500 relative top-4 lg:top-0 lg:self-start justify-end p-2 px-5 text-sm font-bold">
+              READ MORE
+            </button>
           </span>
         </span>
       </div>
@@ -76,7 +85,7 @@ function Top() {
       <div className="bg-blue-950 p-4">
         <h1 className="text-2xl font-bold text-orange-300">New</h1>
         {news.map(({ heading, text, line }) => (
-          <div>
+          <div key={heading}>
             <span className="p-4">
               <h1 className="text-white font-semibold text-xl">{heading}</h1>
               <h3 className="text-white text-sm">{text}</h3>
@@ -90,18 +99,18 @@ function Top() {
 }
 
 function Footer() {
-  return(
+  return (
     <div className="py-8 lg:flex lg:flex-row lg:px-20">
-    {footer.map(({cardImage,number,text1,details})=>(
-      <div className="flex flex-row gap-4 py-3 px-5">
-        <img src={cardImage} className="h-28" alt="" />
-        <div className="flex flex-col gap-2">
-          <h2 className="text-3xl font-bold text-red-400">{number}</h2>
-          <h2 className="font-bold">{text1}</h2>
-          <h2 className="text-[12px] text-gray-500">{details}</h2>
+      {footer.map(({ cardImage, number, text1, details }) => (
+        <div className="flex flex-row gap-4 py-3 px-5" key={number}>
+          <img src={cardImage} className="h-28" alt="" />
+          <div className="flex flex-col gap-2">
+            <h2 className="text-3xl font-bold text-red-400">{number}</h2>
+            <h2 className="font-bold">{text1}</h2>
+            <h2 className="text-[12px] text-gray-500">{details}</h2>
+          </div>
         </div>
-      </div>
-    ))}
+      ))}
     </div>
-  )
+  );
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 
-
 function Nav() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   return (
     <nav className="lg:px-24 lg:p-10 px-5 py-4 flex justify-between items-center">
       {/* logo and hamburger menu */}
@@ -13,33 +12,40 @@ function Nav() {
       </a>
       {/* Menu items listed here */}
       {isMenuOpen && <ListItems toggleMenu={toggleMenu} />}
+
       <span className="hidden lg:flex">
-      <ul className="flex flex-row w-3/4">
-        <a href="#" onClick={toggleMenu} className="hidden">
-          <img src="/assets/images/icon-menu-close.svg" alt="" />
-        </a>
-        <li className="list-none lg:flex lg:flex-row relative lg:gap-5">
-          <a href="#">Home</a>
-          <a href="#">New</a>
-          <a href="#">Popular</a>
-          <a href="#">Trending</a>
-          <a href="#">Categories</a>
-        </li>
-      </ul>
-    </span>
+        <ul className="flex flex-row w-3/4">
+          <a href="#" onClick={toggleMenu} className="hidden">
+            <img src="/assets/images/icon-menu-close.svg" alt="" />
+          </a>
+          <li className="list-none lg:flex lg:flex-row relative lg:gap-5">
+            <a href="#">Home</a>
+            <a href="#">New</a>
+            <a href="#">Popular</a>
+            <a href="#">Trending</a>
+            <a href="#">Categories</a>
+          </li>
+        </ul>
+      </span>
     </nav>
   );
 }
 
 export default Nav;
 
-function ListItems({toggleMenu}) {
+function ListItems({ toggleMenu }) {
   return (
-    <span className="absolute top-0 left-0 bg-[rgba(0,0,0,0.5)] w-full h-full">
+    <span className="fixed top-0 left-0 bg-[rgba(0,0,0,0.5)] w-full h-full z-50">
+      {/* i made it fixed position and made the z-index 50 because you mallam made the button relative so it was showing on the navbar */}
+      {/* {i also added key to all the mapping of props you used cos of Good practice} */}
       <ul className="flex flex-col absolute top-0 left-1/3 h-full w-3/4 p-6 bg-white">
-        <a href="#" onClick={toggleMenu} className="">
-          <img src="/assets/images/icon-menu-close.svg" alt="" />
-        </a>
+        <div className="flex justify-between items-center mb-4 px-8">
+          <div></div>
+          <a href="#" onClick={toggleMenu}>
+            <img src="/assets/images/icon-menu-close.svg" alt="Close Menu" />
+          </a>
+        </div>
+        {/* also for here, your close button was floating around, so i did the above to make it that way, they are better wasys to do this but i wanted to try this method Y */}
         <li className="list-none flex flex-col relative top-12 gap-3">
           <a href="#">Home</a>
           <a href="#">New</a>
